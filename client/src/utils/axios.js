6b26dd5a0c6fa85ab64aeba31448ee9a5ef5451a
Joseph Whittington <joseph.a.whittington@gmail.com>
2019-04-02 19:02:59 -0400

    sets the baseURL in axios monolith to different urls for staging and prod

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,14 +1,19 @@
-import axios from 'axios';
+import axios from "axios";
+
+const setBaseURL = () =>
+    process.env.NODE_ENV === "development"
+        ? "http://localhost:4000"
+        : "https://onlineshitlist.com/api";
 
 const Axios = axios.create({
-  baseURL: 'http://localhost:4000',
-  timeout: 850,
-  headers: {
-    authorization: `Bearer ${
-      localStorage.getItem('token') ? localStorage.getItem('token') : null
-    }`,
-    "access-control-allow-origin": "*"
-  }
+    baseURL: setBaseURL(),
+    timeout: 850,
+    headers: {
+        authorization: `Bearer ${
+            localStorage.getItem("token") ? localStorage.getItem("token") : null
+        }`,
+        "access-control-allow-origin": "*"
+    }
 });
 
 export default Axios;
