d1f3f94068b8b03ffda3b7035b388d352f783379
Joseph Whittington <joseph.a.whittington@gmail.com>
2019-04-05 19:10:09 -0400

    updates tweet api and tweet component

--- client/src/components/Tweet.js
+++ client/src/components/Tweet.js
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 
 import Card from "react-bootstrap/Card";
 import Badge from "react-bootstrap/Badge";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Button from "react-bootstrap/Button";
 
 const setBaseURL = () =>
     process.env.NODE_ENV === "development"
@@ -11,7 +13,14 @@ const setBaseURL = () =>
 
 export default class Tweet extends Component {
     render() {
-        let { body, username, likedBy, comments, media } = this.props.props;
+        let {
+            body,
+            username,
+            displayName,
+            likedBy,
+            comments,
+            media
+        } = this.props.props;
         const time = new Date(this.props.props.timestamp);
         const localtime = time.toLocaleTimeString();
         const localdate = time.toLocaleDateString();
@@ -19,25 +28,30 @@ export default class Tweet extends Component {
             media = `${setBaseURL()}/${media.split("\\")[2]}`;
         return (
             <Card className="my-3 w-50 mx-auto">
-                <Card.Header className="bg-white border-bottom-0">
-                    <span
-                        style={{
-                            height: 20,
-                            width: 20,
-                            backgroundColor: "black"
-                        }}
-                        className="d-inline-block my-auto rounded-circle mr-2"
-                    />
-                    <Link
-                        className="d-inline-block text-dark my-auto"
-                        to={`/user/${username}`}
-                    >
-                        <span className="pr-3">
-                            <Badge variant="secondary">@{username}</Badge>
-                        </span>
-                    </Link>
-                    <br />
-                    <small>
+                <Card.Header className="bg-white border-light">
+                    <div className="d-flex align-items-center">
+                        <span
+                            style={{
+                                height: 20,
+                                width: 20,
+                                backgroundColor: "black"
+                            }}
+                            className="d-inline-block my-auto rounded-circle mr-2"
+                        />
+                        <Link className="text-dark" to={`/user/${username}`}>
+                            <span className="h4 m-0">
+                                <span className="pr-2">{displayName}</span>
+                            </span>
+                        </Link>
+                        <Link className="text-dark" to={`/user/${username}`}>
+                            <span className="text-dark pr-3">
+                                <Badge variant="info" className="">
+                                    @{username}
+                                </Badge>
+                            </span>
+                        </Link>
+                    </div>
+                    <small className="text-grey">
                         On {localdate} @{localtime}
                     </small>
                 </Card.Header>
@@ -51,11 +65,12 @@ export default class Tweet extends Component {
                     )}
                     <Card.Text className="text-dark">{body}</Card.Text>
                 </Card.Body>
-                <Card.Footer className="d-flex bg-white border-top-0 justify-content-between">
-                    <span>
-                        <Card.Link href="#">Retweet</Card.Link>
-                        <Card.Link href="#">({likedBy.length})Like</Card.Link>
-                    </span>
+                <Card.Footer className="p-0 bg-white rounded-top">
+                    <ButtonGroup className="w-100 rounded-top">
+                        <Button className="">Retweet</Button>
+                        <Button className="">({comments.length})Comment</Button>
+                        <Button className="">({likedBy.length})Like</Button>
+                    </ButtonGroup>
                 </Card.Footer>
             </Card>
         );
