dbada21017ace0fcd2609aec320cd59f3b6c2c3e
Joseph Whittington <joseph.a.whittington@gmail.com>
2019-04-06 11:37:55 -0400

    hahhah

--- client/src/components/CodeBlock.js
+++ client/src/components/CodeBlock.js
@@ -5,12 +5,12 @@ export default class CodeBlock extends Component {
         return (
             <div className="w-50 mx-auto">
                 <h3>Code:</h3>
-                <pre className="p-3 bg-dark text-light rounded">
+                <pre
+                    style={{ tabSize: 4 }}
+                    className="p-3 bg-dark text-light rounded"
+                >
                     <code>
-                        {`function numberAdd(str) {
-const nums = str.match(/\d+/g);
-return nums.reduce((accum, value, index) => accum + parseInt(value), 0);
-}\n`}
+                        {`function numberAdd(str) \n\tconst nums = str.match(/\d+/g);\n\treturn nums.reduce((accum, value, index) => accum + parseInt(value), 0);\n}`}
                     </code>
                 </pre>
             </div>
