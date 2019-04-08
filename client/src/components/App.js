7744489c7b521403b7f820abe4bef50fd43ad224
Joseph Whittington <joseph.a.whittington@gmail.com>
2019-04-07 23:28:36 -0400

    adds 404 page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Home from "./Home";
 import Login from "./Login";
 import Register from "./Register";
@@ -11,14 +11,14 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <div id="main-section">
+                <Switch>
                     <Route path="/user/:userid" component={UserHome} />
                     <Route exact path="/login" component={Login} />
                     <Route path="/register" component={Register} />
                     <Route path="/feed" component={Feed} />
                     <Route exact path="/" component={Home} />
                     <Route component={NotFound} />
-                </div>
+                </Switch>
             </div>
         );
     }
